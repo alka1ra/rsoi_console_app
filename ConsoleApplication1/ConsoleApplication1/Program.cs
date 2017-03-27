9be4b0fd30a2fcbb4cb210d655d47be6c16bcc22
alka_ra <alyarog@gmail.com>
2017-03-27 22:37:34 +0300

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private String department;
+        private int quantity;
+
+        Program(String department, int quantity)
+        {
+            this.department = department;
+            this.quantity = quantity;
+        }
+
+        static void Main(string[] args)
+        {
+            Boolean t = true;
+
+            while (t)
+            {
+                Console.WriteLine("Choose an option:\n 1 - Enter new record\n 2 - View all the data\n 3 - View subdivisin's data\n" + " 4 - Remove record\n 5 - Edit record\n 6 - Sort by subdevision\n 7 - Exit");
+                String choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Enter subdivision");
+                            String subdevision = Console.ReadLine();
+                            Console.WriteLine("Enter quantity of employees");
+                            int quantity = Int32.Parse(Console.ReadLine());
+                            Program obj = new Program(subdevision, quantity);
+                            FileStream fstr = new FileStream(@"D:\workspace\c_sharp\files\lb5\file.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                            fstr.Seek(0, SeekOrigin.End);
+                            StreamWriter sw = new StreamWriter(fstr);
+                            sw.WriteLine(obj.department + " " + obj.quantity.ToString() + " ");
+                            sw.Close();
+                            fstr.Close();
+                            break;
+                        }
+                    case "2":
+                        {
+                            string text = System.IO.File.ReadAllText(@"D:\workspace\c_sharp\files\lb5\file.txt");
+                            System.Console.WriteLine("All data of application is:\n{0}", text);
+                            break;
+                        }
+
+                    case "3":
+                        {
+                            string line;
+                            int quant = 0;
+                            Console.WriteLine("Enter number of subdivision:\n 01 - Accounting\n 02 - HRD\n 03 - PD\n 04 - R&D\n");
+                            String department = Console.ReadLine();
+                            System.IO.StreamReader file = new System.IO.StreamReader(@"D:\workspace\c_sharp\files\lb5\file.txt");
+
+
+                            switch (department)
+                            {
+
+                                case "01":
+                                    {
+                                        while ((line = file.ReadLine()) != null)
+                                        {
+                                            string[] separators = { " " };
+                                            string[] words = line.Split(separators, StringSplitOptions.None);
+                                            if (words[0].Equals("Accounting"))
+                                            {
+                                                quant += Int32.Parse(words[1]);
+                                            }
+                                        }
+                                        Console.WriteLine("Accounting " + quant);
+                                        break;
+                                    }
+                                case "02":
+                                    {
+                                        while ((line = file.ReadLine()) != null)
+                                        {
+                                            string[] separators = { " " };
+                                            string[] words = line.Split(separators, StringSplitOptions.None);
+                                            if (words[0].Equals("HRD"))
+                                            {
+                                                quant += Int32.Parse(words[1]);
+                                            }
+                                        }
+                                        Console.WriteLine("HRD " + quant);
+                                        break;
+                                    }
+                                case "03":
+                                    {
+                                        while ((line = file.ReadLine()) != null)
+                                        {
+                                            string[] separators = { " " };
+                                            string[] words = line.Split(separators, StringSplitOptions.None);
+                                            if (words[0].Equals("PD"))
+                                            {
+                                                quant += Int32.Parse(words[1]);
+                                            }
+                                        }
+                                        Console.WriteLine("PD " + quant);
+                                        break;
+                                    }
+                                case "04":
+                                    {
+                                        while ((line = file.ReadLine()) != null)
+                                        {
+                                            string[] separators = { " " };
+                                            string[] words = line.Split(separators, StringSplitOptions.None);
+                                            if (words[0].Equals("R&D"))
+                                            {
+                                                quant += Int32.Parse(words[1]);
+                                            }
+                                        }
+                                        Console.WriteLine("R&D " + quant);
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                    case "4":
+                        {
+                            string text = System.IO.File.ReadAllText(@"D:\workspace\c_sharp\files\lb5\file.txt");
+                            System.Console.WriteLine("All data of application is:\n{0}", text);
+                            Console.WriteLine("Enter number of the record to be deleted: \n");
+                            int d = Int32.Parse(Console.ReadLine()) - 1;
+
+                            string[] separators = { "\n" };
+                            string[] lines = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                            int kol = lines.Length;
+
+                            using (var file = new StreamWriter(@"D:\workspace\c_sharp\files\lb5\file.txt"))
+                            {
+                                for (int i = 0; i < kol; i++)
+                                {
+                                    if (i != d)
+                                    {
+                                        file.WriteLine(lines[i]);
+                                    }
+                                }
+                            }
+                            text = System.IO.File.ReadAllText(@"D:\workspace\c_sharp\files\lb5\file.txt");
+                            System.Console.WriteLine("All data of application is:\n{0}", text);
+                            break;
+                        }
+                    case "5":
+                        {
+                            string text = System.IO.File.ReadAllText(@"D:\workspace\c_sharp\files\lb5\file.txt");
+                            System.Console.WriteLine("All data of application is:\n{0}", text);
+                            Console.WriteLine("Enter number of the record to be edited: \n");
+                            int d = Int32.Parse(Console.ReadLine()) - 1;
+
+                            string[] separators = { "\n" };
+                            string[] lines = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                            int kol = lines.Length;
+
+                            Console.WriteLine("Enter new record: \n" + " Enter subdivision: \n");
+                            String subdevision = Console.ReadLine();
+                            Console.WriteLine("Enter quantity of employees");
+                            int quantity = Int32.Parse(Console.ReadLine());
+
+                            lines[d] = subdevision + " " + quantity.ToString();
+
+                            using (var file = new StreamWriter(@"D:\workspace\c_sharp\files\lb5\file.txt"))
+                            {
+                                for (int i = 0; i < kol; i++)
+                                {
+                                    {
+                                        file.WriteLine(lines[i]);
+                                    }
+                                }
+                            }
+                            text = System.IO.File.ReadAllText(@"D:\workspace\c_sharp\files\lb5\file.txt");
+                            System.Console.WriteLine("All data of application is:\n{0}", text);
+                            break;
+                        }
+                    case "6":
+                        {
+                            List<Int32> accounting = new List<Int32>();
+                            List<Int32> hrd = new List<Int32>();
+                            List<Int32> pd = new List<Int32>();
+                            List<Int32> r_d = new List<Int32>();
+                            string line;
+                            Console.WriteLine("Enter the number of subdivision you want to sort:\n 01 - Accounting\n 02 - HRD\n 03 - PD\n 04 - R&D\n");
+                            String department = Console.ReadLine();
+                            System.IO.StreamReader file = new System.IO.StreamReader(@"D:\workspace\c_sharp\files\lb5\file.txt");
+
+                            switch (department)
+                            {
+                                case "01":
+                                    {
+                                        while ((line = file.ReadLine()) != null)
+                                        {
+                                            string[] separators = { " " };
+                                            string[] words = line.Split(separators, StringSplitOptions.None);
+                                            if (words[0].Equals("Accounting"))
+                                            {
+                                                accounting.Add(Int32.Parse(words[1]));
+                                            }
+                                        }
+                                        accounting.Sort();
+                                        Console.Write("Accounting: " + "\n");
+                                        foreach (var record in accounting)
+                                            Console.Write("   {0}", record + "\n");
+                                        break;
+                                    }
+                                case "02":
+                                    {
+                                        while ((line = file.ReadLine()) != null)
+                                        {
+                                            string[] separators = { " " };
+                                            string[] words = line.Split(separators, StringSplitOptions.None);
+                                            if (words[0].Equals("HRD"))
+                                            {
+                                                hrd.Add(Int32.Parse(words[1]));
+                                            }
+                                        }
+                                        hrd.Sort();
+                                        Console.Write("HRD: " + "\n");
+                                        foreach (var record in hrd)
+                                            Console.Write("   {0}", record + "\n");
+                                        break;
+                                    }
+                                case "03":
+                                    {
+                                        while ((line = file.ReadLine()) != null)
+                                        {
+                                            string[] separators = { " " };
+                                            string[] words = line.Split(separators, StringSplitOptions.None);
+                                            if (words[0].Equals("PD"))
+                                            {
+                                                pd.Add(Int32.Parse(words[1]));
+                                            }
+                                        }
+                                        pd.Sort();
+                                        Console.Write("PD: " + "\n");
+                                        foreach (var record in pd)
+                                            Console.Write("   {0}", record + "\n");
+                                        break;
+                                    }
+                                case "04":
+                                    {
+                                        while ((line = file.ReadLine()) != null)
+                                        {
+                                            string[] separators = { " " };
+                                            string[] words = line.Split(separators, StringSplitOptions.None);
+                                            if (words[0].Equals("R&D"))
+                                            {
+                                                r_d.Add(Int32.Parse(words[1]));
+                                            }
+                                        }
+                                        pd.Sort();
+                                        Console.Write("R&D: " + "\n");
+                                        foreach (var record in r_d)
+                                            Console.Write("   {0}", record + "\n");
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+                    case "7":
+                        {
+                            t = false;
+                            Console.WriteLine("Press any key to exit.");
+                            System.Console.ReadKey();
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApplicationStudents/ConsoleApplicationStudents/Program.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplicationStudents
+{
+
+    class Program
+    {
+        private static string subdivision = "subjects";
+        private static string quantity = "absences";
+        private static string filePath = "D:\\workspace\\c_sharp\\files\\lb5\\file_students.txt";
+        private static string[] subdevisions = { "Maths", "Programming", "Humanities", "Science" };
+
+
+        static void Main(string[] args)
+        {
+            run();
+        }
+        
+
+        static void showMenu()
+        {
+            Console.WriteLine("Choose an option:\n" +
+                " 1 - Enter new record\n" +
+                " 2 - View all the data\n" +
+                " 3 - View data of {0}\n" +
+                " 4 - Remove record\n" +
+                " 5 - Edit record\n" +
+                " 6 - Sort by {0}\n" +
+                " 7 - Exit", subdivision);
+        }
+
+        static void run()
+        {
+            bool t = true;
+            while (t)
+            {
+                showMenu();
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                    {
+                        t = enterNewRecord(); break;
+                    }
+                    case "2":
+                    {
+                        t = viewAllTheData(); break;
+                    }
+                    case "3":
+                    {
+                        t = viewSubdivData(); break;
+                    }
+                    case "4":
+                    {
+                        t = removeRecord(); break;
+                    }
+                    case "5":
+                    {
+                        t = editRecord(); break;
+                    }
+                    case "6":
+                    {
+                        t = sortBySubdevision(); break;
+                    }
+                    case "7":
+                    {
+                        t = exit(); break;
+                    }
+                }
+            }
+        }
+
+        static bool enterNewRecord()
+        {
+            string stringToWrite = getRecord();
+            writeStringInFile(stringToWrite);
+
+            Console.WriteLine("A new record has been entereed.\n");
+            return true;
+        }
+
+        static bool viewAllTheData()
+        {
+            string[] text = getAllDataFromFile();
+            Console.WriteLine("All data of application is:\n");
+            printLines(text);
+
+            Console.WriteLine("\n");
+                            
+            return true;
+        }
+
+        static bool viewSubdivData() 
+        {
+            Console.WriteLine("Enter name of {0} you wnt to look through\n", subdivision);
+            string criteria = Console.ReadLine().Trim();
+
+            string[] text = getAllDataFromFile();
+            List<string> result = new List<string>();
+
+            foreach (string s in text) 
+            {
+                if (s.Trim().StartsWith(criteria)) 
+                {
+                    result.Add(s);
+                }
+            }
+
+            printLines(result);
+            Console.WriteLine("\n");
+            
+            return true;
+        }
+       
+        static bool removeRecord()
+        {
+           
+            Console.WriteLine("Enter number of the record to be deleted: \n");
+            int d = Int32.Parse(Console.ReadLine()) - 1;
+
+            string[] lines = getAllDataFromFile();
+            int kol = lines.Length;
+
+            if (d > kol)
+            {
+                Console.WriteLine("There is no record with such number.");
+            }
+            else
+            {
+                File.WriteAllText(filePath, string.Empty);
+                writeStringsInFile(lines, 0, d);
+                writeStringsInFile(lines, d+1, kol);
+            }
+            Console.WriteLine("Record has been removed.");
+            Console.WriteLine("\n");
+            return true;
+        }
+
+        static bool sortBySubdevision()
+        {          
+            string[] text = getAllDataFromFile();
+
+            List<string> subdivItems = text.ToList();
+            subdivItems.Sort();
+
+
+            File.WriteAllText(filePath, string.Empty);
+            writeStringsInFile(subdivItems);
+
+            Console.WriteLine("Items have been sort by {0}.\n", subdivision);
+            
+            return true;
+        }
+
+        static bool editRecord()
+        {
+            Console.WriteLine("Enter number of the record to be edited: \n");
+            int d = Int32.Parse(Console.ReadLine()) - 1;
+
+            string[] lines = getAllDataFromFile();
+            int kol = lines.Length;
+
+            if (d > kol)
+            {
+                Console.WriteLine("There is no record with such number.");
+                Console.WriteLine("\n");
+            }
+            else
+            {
+                Console.WriteLine("Enter new record: \n");
+                lines[d] = getRecord();
+                File.WriteAllText(filePath, string.Empty);
+                writeStringsInFile(lines, 0, kol);
+                Console.WriteLine("\n");
+            }
+            return true;
+        }
+
+        static bool exit()
+        {
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+            return false;
+        }
+
+        static void writeStringInFile(string line)
+        {
+            using (StreamWriter file = new StreamWriter(filePath, true))
+            {
+                file.WriteLine(line);
+            }
+        }
+
+        static void writeStringsInFile(List<string> list)
+        {
+            using (StreamWriter file = new StreamWriter(filePath, true))
+            {
+                foreach(string s in list)
+                {
+                    file.WriteLine(s);
+                }
+            }
+        }
+
+        static void writeStringsInFile(string[] strings, int first, int last) 
+        {
+            if (first < last) 
+            {
+                using (StreamWriter file = new StreamWriter(filePath, true))
+                {
+                    for (int i = first; i < last; i++)
+                    {
+                        file.WriteLine(strings[i]);
+                    }
+
+                }
+            }
+        }
+
+        static string[] getAllDataFromFile()
+        {
+            string text = System.IO.File.ReadAllText(filePath);
+            string[] separators = { "\n" };
+            return text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        static void printLines(string[] lines)
+        {
+            foreach (string s in lines)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static void printLines(List<string> lines)
+        {
+            foreach (string s in lines)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static string getRecord() 
+        {
+            Console.WriteLine("Enter " + subdivision);
+            string inputSubdevision = Console.ReadLine();
+            Console.WriteLine("Enter " + quantity);
+            int inputQuantity = Int32.Parse(Console.ReadLine());
+
+            return inputSubdevision + " " + inputQuantity.ToString() + " ";
+        }
+    }
+}
